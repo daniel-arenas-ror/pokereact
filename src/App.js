853803db189259f8cff1f3321624b72967bea1c9
Daniel Arenas <darenas@pibox.app>
2025-07-13 18:12:35 -0500

    update useDispatch

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import './App.css';
 import { Col } from 'antd';
 import Searcher from './components/Searcher';
 import PokemonList from './components/PokemonList';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { getPokemon } from './api';
-import { connect } from 'react-redux';
-import { setPokemons as setPokemonsActions } from './actions';
+import { setPokemons } from './actions';
+import { useDispatch, useSelector } from 'react-redux';
 
-function App({ pokemons, setPokemons }) {
+function App() {
+  const pokemons = useSelector(state => state.pokemons);
+  const dispatch = useDispatch();
+
   useEffect(() => {
     const fetchPokemont = async () => {
       const pokemonsRes = await getPokemon();
-      setPokemons(pokemonsRes)
+      dispatch(setPokemons(pokemonsRes));
     };
 
     fetchPokemont();
@@ -31,12 +34,4 @@ function App({ pokemons, setPokemons }) {
   );
 }
 
-const mapStateToProps = (state) => ({
-  pokemons: state.pokemons
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  setPokemons: (value) => dispatch(setPokemonsActions(value))
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
