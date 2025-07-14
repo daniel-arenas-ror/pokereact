6eb4da467de746bb1dc4f05ef4acba51de8fc51e
Daniel Arenas <darenas@pibox.app>
2025-07-13 19:39:16 -0500

    add custom middlewares

--- src/index.js
+++ src/index.js
@@ -4,10 +4,20 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { pokemonsReducer } from './reducers/pokemons';
-import { legacy_createStore as createStore } from 'redux';
+import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux';
+import { logger, featuring } from './middlewares';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const store = createStore(pokemonsReducer);
+
+const composedEnhancers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger, featuring)
+)
+
+const store = createStore(
+  pokemonsReducer,
+  composedEnhancers
+);
 
 root.render(
   <React.StrictMode>
