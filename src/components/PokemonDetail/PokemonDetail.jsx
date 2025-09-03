06f8062fcd0da6f012f57a1d21a36792e58d6f04
Daniel Arenas <darenas@pibox.app>
2025-09-03 12:49:58 +0100

    simulate loading

--- src/components/PokemonDetail/PokemonDetail.jsx
+++ src/components/PokemonDetail/PokemonDetail.jsx
@@ -1,39 +1,90 @@
 import { useParams } from 'react-router';
 import { getPokemonDetailsByName } from '../../api';
 import { useState, useEffect } from 'react';
-import { Layout, Grid, Col, Row } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
+import { setLoading } from "../../slices/uiSlice";
+import { Layout, Col, Row, Divider, Progress, Flex } from 'antd';
+import { Spin } from 'antd';
+import { Tag } from 'antd';
 import './style.css';
 
 const PokemonDetail = () => {
   const { name } = useParams();
-  const { Header, Footer, Content } = Layout;
+  const loading = useSelector(state => state.ui.loading);
+  const dispatch = useDispatch();
+  const { Header, Footer } = Layout;
+  const [percent, setPercent] = useState(0);
 
   const [pokemonDetails, setPokemonDetails] = useState(null);
 
   useEffect(() => {
-    getPokemonDetailsByName(name).then(setPokemonDetails);
+    dispatch(setLoading(true));
+    setTimeout(function(){
+      getPokemonDetailsByName(name).then(setPokemonDetails);
+      let ptg = -10;
+
+      const interval = setInterval(() => {
+        ptg += 5;
+        setPercent(ptg);
+  
+        if (ptg > 120) {
+          clearInterval(interval);
+          dispatch(setLoading(false));
+          setPercent(0);
+        }
+      }, 100);
+    }, 2000);
   }, [name]);
 
-  if (!pokemonDetails) {
+  if (!pokemonDetails && !loading) {
     return <div>Pokemon {name} not found </div>;
+  }
+
+  if (loading) {
+    return <Spin spinning={loading} percent={percent} fullscreen />;
   }
 
   return (
     <Layout>
-      <Header><h2>{pokemonDetails.name}</h2></Header>
+      <h1><Divider>{pokemonDetails.name}</Divider></h1>
       <Row justify="center" align="top">
         <Col span={6} >
           <img src={pokemonDetails.sprites.front_default} alt={pokemonDetails.name}  />
           <div className="statistics">
-            <p>statistics</p>
+            <Flex gap="small" vertical>
+              <Progress percent={30} status="active" />
+              <Progress percent={50} status="active" />
+              <Progress percent={70} status="exception" />
+              <Progress percent={100} status="active"/>
+              <Progress percent={50} status="active" />
+            </Flex>
           </div>
         </Col>
         <Col span={6} >
           <div className="types">
-            <p>Types:</p>
+            <Divider orientation="left">Types</Divider>
+            <Flex gap="4px 0" wrap>
+              <Tag color="magenta">magenta</Tag>
+              <Tag color="red">red</Tag>
+              <Tag color="volcano">volcano</Tag>
+              <Tag color="orange">orange</Tag>
+              <Tag color="gold">gold</Tag>
+              <Tag color="lime">lime</Tag>
+              <Tag color="green">green</Tag>
+              <Tag color="cyan">cyan</Tag>
+              <Tag color="blue">blue</Tag>
+              <Tag color="geekblue">geekblue</Tag>
+              <Tag color="purple">purple</Tag>
+            </Flex>
           </div>
           <div className="weaknesses">
-            <p>Weaknesses:</p>
+            <Divider orientation="left">Weaknesses</Divider>
+            <Flex gap="4px 0" wrap>
+              <Tag color="#f50">#f50</Tag>
+              <Tag color="#2db7f5">#2db7f5</Tag>
+              <Tag color="#87d068">#87d068</Tag>
+              <Tag color="#108ee9">#108ee9</Tag>
+            </Flex>
           </div>
         </Col>
       </Row>
